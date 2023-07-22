0c0aff55d39df5840d8a42929c115ceda4692367
TeRisha12 <127959998+TeRisha12@users.noreply.github.com>
2023-07-22 09:15:20 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "./NewProject.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> This is my first react project</h1>
+      <h1>Pratima Adhikary</h1>
     </div>
   );
 }
-
 export default App;
---
+++ src/NewProject.js
@@ -0,0 +1,16 @@
+function Project() {
+  return (
+    <div>
+      <p style={{ fontSize: "50px", fontStyle: "Bold" }}>
+        About our country Nepal{" "}
+      </p>
+      <h1>Our country is rich in natural resourses</h1>
+      <h2>It is the birth place of gautam buddha</h2>
+      <h3>Nepali eat dal bhat daily</h3>
+      <h4>arranges marriages are the norm in nepal</h4>
+      <h5>The nepalese flag is Very unique</h5>
+    </div>
+  );
+}
+
+export default Project;
